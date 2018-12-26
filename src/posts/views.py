ba118c75c302253bfdc3ffeaf7c111d627e2f0be
“Vineeth” <k.vineeth26@gmail.com>
2018-12-26 19:55:54 +0530

    21th one Completed and Working

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render,get_object_or_404
 
 # Create your views here.
@@ -9,8 +9,9 @@ def post_create(request):
     form = PostForm(request.POST or None)
     if form.is_valid():
         instance = form.save(commit=False)
-        print (form.cleaned_data.get("title"))
         instance.save()
+        #This line is added
+        return HttpResponseRedirect(instance.get_absolute_url())
     context = {
         "form":form,
     }
@@ -32,8 +33,21 @@ def post_list(request):
     }
     return render(request, "index.html", context)
 
-def post_update(request):
-    return HttpResponse("<h1>Update</h1>")
+def post_update(request, id =None):
+    instance = get_object_or_404(Post, id=id)
+    # instance=instance is to be added to retrive data to form.
+    # Here insted of queryset we used instance which is dict
+    form = PostForm(request.POST or None,instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        return HttpResponseRedirect(instance.get_absolute_url())
+    context ={
+    "title": instance.title,
+    "instance":instance,
+    "form":form,
+    }
+    return render(request, "post_form.html", context)
 
 def post_delete(request):
     return HttpResponse("<h1>Delete</h1>")
