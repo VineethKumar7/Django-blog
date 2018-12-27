54a2d9b18c42d9d1e842419db54b7bd41562d9a9
“Vineeth” <k.vineeth26@gmail.com>
2018-12-27 23:30:05 +0530

    27 completed

--- src/posts/models.py
+++ src/posts/models.py
@@ -5,13 +5,12 @@ from django.core.urlresolvers import reverse
 class Post(models.Model):
     title = models.CharField(max_length = 120)
     content = models.TextField()
-    # Auto now means that evey single time it is saved in database.
     updated = models.DateTimeField(auto_now= True, auto_now_add = False)
-    # auto_now_add just updates only when we add items into this field.
     timestap = models.DateTimeField(auto_now= False, auto_now_add = True)
-
     def __str__(self):
         return self.title
-
     def get_absolute_url(self):
         return reverse("posts:detail",kwargs={"id":self.id})
+    #This makes sure that all the datas are in this order.
+    class Meta:
+        ordering = ["-timestap", "-updated"]
