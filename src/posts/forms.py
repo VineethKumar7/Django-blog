1d139c986d23f6c4d6b8ae425739b714d1f12f9f
“Vineeth” <k.vineeth26@gmail.com>
2019-01-06 23:37:12 +0530

    Dynamic preview of the form

--- src/posts/forms.py
+++ src/posts/forms.py
@@ -2,8 +2,9 @@ from django import forms
 from .models import Post
 from pagedown.widgets import PagedownWidget
 class PostForm(forms.ModelForm):
-    # We need to add this content variable here
-    content = forms.CharField(widget=PagedownWidget)
+    # The PagedownWidget ischanged to PagedownWidget(show_preview = False)
+    # Then we no longer view the image at edit view.  
+    content = forms.CharField(widget=PagedownWidget(show_preview = False))
     publish = forms.DateField(widget = forms.SelectDateWidget)
     class Meta:
         model = Post
